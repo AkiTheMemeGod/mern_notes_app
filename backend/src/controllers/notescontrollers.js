2a974872de2db927e185931999b891ae41e3797d
AkiTheMemeGod <k.akashkumar@gmail.com>
2025-09-03 01:10:31 +0530

    Implement note model and CRUD operations in the notes controller

--- backend/src/controllers/notescontrollers.js
+++ backend/src/controllers/notescontrollers.js
@@ -1,15 +1,55 @@
-export function getNotes(req, res){
-    res.status(200).send("You've Got 20 notes!");
+import Note from "../models/Note.js";
+
+export async function getNotes(req, res){
+    try{
+        const notes = await Note.find();
+        res.status(200).json(notes);
+    }
+    catch(error){
+        console.error("Error fetching notes:", error);
+        res.status(500).json({"message": "Server error"});
+    }
 }
 
-export function createNote(req, res){
-    res.status(201).send("Note created");
+export async function createNote(req, res){
+    try{
+        const {title, content} = req.body
+        const newNote = new Note({title, content})
+        await newNote.save()
+        res.status(201).json({"message": "Note created", note: newNote})
+    }
+    catch(error){
+        console.error("Error creating note:", error);
+        res.status(500).json({"message": "Server error"});
+    }
 }
 
-export function updateNote(req, res){
-    res.status(200).send("Note updated");
+export async function updateNote(req, res){
+    try{
+        const {title, content} = req.body
+        const updatedNote = await Note.findByIdAndUpdate(
+            req.params.id,
+            {title, content},
+            {
+            new: true,
+        }
+        );
+        if(!updatedNote) return res.status(404).json({"message": "Note not found"})
+        res.status(200).json({"message": "Note updated", note: Note})
+    }
+    catch(error){
+        console.error("Error updating note:", error);
+        res.status(500).json({"message": "Server error"});
+    }
 }
 
-export function deleteNote(req, res){
-    res.status(204).send("Note deleted");
-}
+export async function deleteNote(req, res){
+    try{
+        const deletedNote = await Note.findByIdAndDelete(req.params.id)
+        if (!deletedNote) return res.status(404).json({"message": "Note not found"})
+        res.status(200).json({"message": "Note Deleted", note: Note})
+    }
+    catch(error){
+        console.error("Error Deleting note:", error);
+        res.status(500).json({"message": "Server error"});
+    }}
